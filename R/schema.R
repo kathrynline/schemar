68117b102204bb314723852a3a096b724b2303e3
kathrynline <elineb@uw.edu>
2022-02-21 22:16:55 -0800

    Adding validate method to schema

--- R/schema.R
+++ R/schema.R
@@ -6,12 +6,21 @@ Schema = R6::R6Class(
     optional_columns = NULL,
 
     initialize = function(required_columns, optional_columns = NULL){
+      # TODO: Assert each element of the list is a Column
       stopifnot(class(required_columns) == "list")
       if (!is.null(optional_columns)){
         stopifnot(class(optional_columns) == "list")
       }
       self$required_columns = required_columns
       self$optional_columns = optional_columns
+    },
+
+    validate = function(data){
+      # Check that required columns are present
+      missing_columns = names(self$required_columns)[!names(self$required_columns) %in% names(data)]
+      if (length(missing_columns)>0){
+        stop(sprintf("Missing required columns %s", paste0(missing_columns, collapse = ", ")))
+      }
     }
   )
 )
