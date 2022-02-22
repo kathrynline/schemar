7fd01386cd92e86196b70973e4b5dede62d67889
kathrynline <elineb@uw.edu>
2022-02-21 21:55:06 -0800

    Updating license and writing first documentation

--- R/column.R
+++ R/column.R
@@ -1,3 +1,10 @@
+#' @title Column
+#' @description Column class
+#'
+#' @field data_type [character] One of integer, numeric, character, or logical.
+#' @field allow_na [logical] Should NAs be allowed in this column?
+#'
+#' @return Schema class instance
 Column = R6::R6Class(
   "Column",
   list(
@@ -6,6 +13,9 @@ Column = R6::R6Class(
 
     # TODO: Think about types of column validations you could want
 
+    #' @description Initializes schema class.
+    #' @param data_type [character] One of integer, numeric, character, or logical.
+    #' @param allow_na [logical] Should NAs be allowed in this column?
     initialize = function(data_type, allow_na = FALSE){
       if (!data_type %in% c('integer', 'numeric', 'character', 'logical')){
         stop(sprintf("Allowed data types are integer, numeric, character, and logical; was passed %s", data_type))
